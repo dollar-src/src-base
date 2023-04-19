8d36a1fe42a3eb9192778474f3c30c85ce9381df
dollar-src <tezcanuyar35@gmail.com>
2023-04-19 19:25:44 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 version "0.9"
-client_script {
-    "config.lua",
-	'client.lua',
-}
 
-server_script {
-	"config.lua",
-	"server.lua",
-	"log.lua",
+shared_scripts {
+    'config.lua'
+}
+server_scripts {
+	'server/*.lua'
+} 
+client_scripts {
+    '*.lua'
 }
