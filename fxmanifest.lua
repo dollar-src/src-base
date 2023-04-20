39b5c144a695c2f094e2bd339f9acab7de243599
dollar-src <tezcanuyar35@gmail.com>
2023-04-20 06:08:16 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,21 @@
 fx_version 'cerulean'
+
+author '$$$#1337 / dollar-src'
+
+description 'SRC-BASE FOR QBCORE'
+
 game 'gta5'
+
 version "1.0"
 
 shared_scripts {
     'config.lua'
 }
+
 server_scripts {
 	'server/*.lua'
 } 
+
 client_scripts {
     'client/*.lua'
 }
